3b675574e68fcf29363e2c007dcdedf617ba4070
KGPROJECT <89075931+kgproject1010@users.noreply.github.com>
2021-08-23 01:52:10 +0700

    Add files via upload

---
+++ KGmusic/config.py
@@ -0,0 +1,47 @@
+# DAISYXMUSIC- Telegram bot project
+# Copyright (C) 2021  Roj Serbest
+# Copyright (C) 2021  Inuka Asith
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Modified by Inukaasith
+
+import os
+from os import getenv
+
+from dotenv import load_dotenv
+
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+que = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "rakasupport")
+BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/2137d62ed6a46ab2abf23.png")
+admins = {}
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "KGassistant")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "KGSupportGroup")
+PROJECT_NAME = getenv("PROJECT_NAME", "KG Music")
+OWNER = getenv("OWNER", "@knsgnwn")
+SOURCE_CODE = getenv("SOURCE_CODE", "github.com/kgproject1010/KG-Music")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
+ARQ_API_KEY = getenv("ARQ_API_KEY", None)
+PMPERMIT = getenv("PMPERMIT", None)
+LOG_GRP = getenv("LOG_GRP", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
